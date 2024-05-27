830a2b7248dd978992c19ddf7ed464aab2a04f8f
ShiftDavit <76012637+ShiftDavit@users.noreply.github.com>
2024-05-26 17:22:48 -0700

    Added a function to fill a cell and a way to render the filled cells with empty ones.

--- Classes/Screen.js
+++ Classes/Screen.js
@@ -1,6 +1,17 @@
 const EMPTY_CELL = "•";
 const FILLED_CELL = "#";
 
+/*
+    5x5 grid example
+    grid = [
+        [*,*,*,*,*],
+        [*,*,*,*,*],
+        [*,*,*,*,*],
+        [*,*,*,*,*],
+        [*,*,*,*,*]
+    ]
+*/
+
 
 class Screen {
     constructor(sizeX, sizeY){
@@ -13,15 +24,27 @@ class Screen {
         }
     }
 
+    fill_cell(x, y){
+        this.grid[y][x] = true;
+    }
+
     toString(){
         
         let output = "";
         let temp_row = "";
+        let current_cell;
 
         for (let Y = 0; Y < this.grid.length; Y++) {
 
             for (let X = 0; X < this.grid[Y].length; X++){
-                temp_row += " " + EMPTY_CELL;
+                switch (this.grid[Y][X]){
+                    case true:
+                        current_cell = FILLED_CELL;
+                        break;
+                    default:
+                        current_cell = EMPTY_CELL;
+                }
+                temp_row += " " + current_cell;
             }
 
             output += temp_row.trim() + "\n";
--- index.js
+++ index.js
@@ -5,4 +5,7 @@ let screen_size_X = 10;
 
 let testScreen = new Screen(screen_size_X, screen_size_Y);
 
+testScreen.fill_cell(0, 0);
+testScreen.fill_cell(5, 5);
+
 process.stdout.write(testScreen.toString());
