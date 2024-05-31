8e4cdce630d5ac89bd8b3dc266ef3e4996b09ef4
ShiftDavit <76012637+ShiftDavit@users.noreply.github.com>
2024-05-31 13:33:26 -0700

    Organized variables and tests

--- app.js
+++ app.js
@@ -2,18 +2,24 @@ const Screen = require("./src/Classes/Screen");
 const InputHandler = require('./src/InputHandler');
 const ComponentRenderer = require('./src/ComponentRenderer');
 const fs = require('fs');
-
-InputHandler.bind_input('space', process.exit);
+const Vector2 = require('./src/Classes/Vector2');
 
 let screen_size_Y = 9;
 let screen_size_X = 16;
 
 let testScreen = new Screen(screen_size_X, screen_size_Y);
+let testComponent = ComponentRenderer.addComponent(new Vector2(2,2), new Vector2(2,2));
 
-let testComponent = ComponentRenderer.spawn(testScreen, [4,5], [2,3]);
+
+
+ComponentRenderer.setScreen(testScreen);
+InputHandler.bind_input('space', process.exit);
+InputHandler.bind_input("right", () => {
+    testComponent.position.x++;
+})
 
 testScreen.update();
 setInterval(() => {
-    testScreen.update();
+    ComponentRenderer.update();
 }, 100);
 
