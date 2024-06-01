58a950d413c1bcc0640e6a347d6e056ac77185b2
ShiftDavit <76012637+ShiftDavit@users.noreply.github.com>
2024-05-31 17:36:43 -0700

    Added left bind

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const Screen = require("./src/Classes/Screen");
 const InputHandler = require('./src/InputHandler');
 const ComponentRenderer = require('./src/ComponentRenderer');
-const fs = require('fs');
 const Vector2 = require('./src/Classes/Vector2');
 
 let screen_size_Y = 9;
@@ -10,16 +9,17 @@ let screen_size_X = 16;
 let testScreen = new Screen(screen_size_X, screen_size_Y);
 let testComponent = ComponentRenderer.addComponent(new Vector2(2,2), new Vector2(2,2));
 
-
-
 ComponentRenderer.setScreen(testScreen);
 InputHandler.bind_input('space', process.exit);
 InputHandler.bind_input("right", () => {
     testComponent.position.x++;
 })
+InputHandler.bind_input("left", () => {
+    testComponent.position.x--;
+})
 
-testScreen.update();
+ComponentRenderer.update();
 setInterval(() => {
     ComponentRenderer.update();
-}, 100);
+}, 0);
 
